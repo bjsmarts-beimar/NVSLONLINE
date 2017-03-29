e2a97e41bb8cfccbfc4c889eb7a0c83764998592
marco chipana <marco@localhost.localdomain>
2017-03-29 11:02:41 -0400

    comments

--- server/python/NVSLOnline_Postgres/NVSLOnline_Django/NVSLOnline/views.py
+++ server/python/NVSLOnline_Postgres/NVSLOnline_Django/NVSLOnline/views.py
@@ -1,16 +1,3 @@
-#from django.shortcuts import render
-from django.http import HttpResponse
-from NVSLOnline.models import Divisions
-#integramo la serializacion de los objetos
-from django.core import serializers
+from django.shortcuts import render
+
 # Create your views here.
-
-def wsDivisions_view(request):
-   #data = serializers.serialize("json", Divisions.objects.filter(IsHidden = False))
-
-    data = serializers.serialize("json", Divisions.objects.filter(IsHidden = False))
-    #retorns data json
-    #return HttpResponse(data, content_type='aplication/json')
-    return HttpResponse(data)
-    #return HttpResponse(data, mimetype='aplication/json')
-    #return HttpResponse({'foo':'bar'})
--- server/python/NVSLOnline_Postgres/NVSLOnline_Django/NVSLOnline_WebService/urls.py
+++ server/python/NVSLOnline_Postgres/NVSLOnline_Django/NVSLOnline_WebService/urls.py
@@ -7,7 +7,7 @@
 
 from django.conf.urls import url, include
 from django.contrib import admin
-#from NVSLOnline_WebService import views
+from NVSLOnline_WebService import views
 urlpatterns = [
     url(r'^api/Divisions/$',views.wsDivisions_view,name="get_list_divisions"),
     #url(r'^admin/', admin.site.urls),
